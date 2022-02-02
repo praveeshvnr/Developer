efa69c637df7a8822e8e9f423c178e62eb9e7fd8
praveeshvnr <ottirypraveesh@gmail.com>
2022-02-02 10:28:23 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.http import request
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def applyleave(request):
+    return render(request,'applyleav.html')
+
